f34627ac188561ba41cf18dc79ee74c490bf24d7
Dherkyan R. Silva <dherkyan@gmail.com>
2024-11-13 01:29:32 -0300

    [Aula/21] 21. Pt.1 - Estrutura da tela Home HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {FormsModule} from "@angular/forms";
 import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './components/menu/menu.component';
 import {MatIconModule} from "@angular/material/icon";
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import {MatIconModule} from "@angular/material/icon";
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    MatIconModule
+    MatIconModule,
+    MatProgressSpinner
   ],
   providers: [
     provideAnimationsAsync()
