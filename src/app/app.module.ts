8b639aed24994e8513d3d28415b9439f12feb68e
Dherkyan R. Silva <dherkyan@gmail.com>
2024-11-14 06:45:09 -0300

    [Aula/30] 30. Pt.3 - Consumindo serviço Firebase e preenchendo tabela de usuários

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { ButtonComponent } from './components/button/button.component';
-import { LoginComponent } from './pages/login/login.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {ButtonComponent} from './components/button/button.component';
+import {LoginComponent} from './pages/login/login.component';
 import {FormsModule} from "@angular/forms";
-import { HomeComponent } from './pages/home/home.component';
-import { MenuComponent } from './components/menu/menu.component';
+import {HomeComponent} from './pages/home/home.component';
+import {MenuComponent} from './components/menu/menu.component';
 import {MatIconModule} from "@angular/material/icon";
 import {MatProgressSpinner} from "@angular/material/progress-spinner";
-import { UsuariosComponent } from './pages/usuarios/usuarios.component';
-import {MatFormField, MatLabel} from "@angular/material/form-field";
-import {MatInput} from "@angular/material/input";
-import {MatTable} from "@angular/material/table";
-import {MatPaginator} from "@angular/material/paginator";
+import {UsuariosComponent} from './pages/usuarios/usuarios.component';
+import {MatFormField, MatFormFieldModule, MatLabel} from "@angular/material/form-field";
+import {MatInput, MatInputModule} from "@angular/material/input";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
+import {HttpClientModule} from "@angular/common/http";
+import {MatSort, MatSortModule} from "@angular/material/sort";
 
 @NgModule({
   declarations: [
@@ -35,12 +37,19 @@ import {MatPaginator} from "@angular/material/paginator";
     MatFormField,
     MatLabel,
     MatInput,
-    MatTable,
-    MatPaginator
+    // MatTable,
+    // MatPaginator,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule
   ],
   providers: [
-    provideAnimationsAsync()
+    provideAnimationsAsync(), MatPaginator, MatSort
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/pages/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -1,6 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {MatTableDataSource} from "@angular/material/table";
 import {Usuario} from "../../models/usuario";
+import {UsuariosService} from "../../services/usuarios.service";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 const ELEMENT_DATA: Usuario[] = [
   {id: 1, username: 'Hydrogen', email: 'alice@gmail.com', bloqueado: false}];
@@ -12,15 +15,54 @@ const ELEMENT_DATA: Usuario[] = [
 })
 export class UsuariosComponent {
 
-  displayedColumns: string[] = ['id', 'username', 'email'];
+  displayedColumns: string[] = ['id', 'username', 'email', 'bloqueado', 'action'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
+  listaDeUsuarios: Usuario[] = [];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  constructor() {
+
+  constructor(private usuarioService: UsuariosService, matPaginator: MatPaginator, matSort: MatSort) {
+    this.dataSource = new MatTableDataSource<Usuario>(this.listaDeUsuarios);
+    this.paginator = matPaginator;
+    this.sort = matSort;
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+    this.listarUsuarios();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  listarUsuarios() {
+    this.usuarioService.listarUsuarios().subscribe({
+      next: responseUsuarios => {
+        console.log('next: ', responseUsuarios);
+        this.listaDeUsuarios = responseUsuarios;
+        this.dataSource = new MatTableDataSource<Usuario>(this.listaDeUsuarios);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: mensagemDeErro => {
+        console.log('error: ', mensagemDeErro);
+      },
+      complete: () => {
+        console.log('complete')
+      }
+    });
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
